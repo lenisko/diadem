effc58bc8e5541503c15dbb5e6b35c50fe54d7fa
lenisko <10072920+lenisko@users.noreply.github.com>
2026-04-29 23:08:21 +0200

    fix: return empty array from /api/koji when Koji not configured

--- src/routes/api/koji/+server.ts
+++ src/routes/api/koji/+server.ts
@@ -1,7 +1,11 @@
 import { error, json } from "@sveltejs/kit";
 import { fetchKojiGeofences } from "@/lib/server/api/kojiApi";
+import { getServerConfig } from "@/lib/services/config/config.server";
 
 export async function GET(event) {
+	const config = getServerConfig();
+	if (!config.koji || !config.koji.url) return json([]);
+
 	const data = await fetchKojiGeofences(event.fetch);
 	if (!data) error(500);
 	return json(data);
